afae080c21699890067bd4e33556b1873908d514
hyojin.sarchet <hyojin5931@hanmail.net>
2017-12-24 17:35:35 -0700

    added google streetview and foursquare info in infowindow

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ var Model = {
     location: {
         lat: 38.920613,
         lng: -77.027349
-      }
+    },
+    FourSquareVenueID: '4a2bf0f2f964a520f6961fe3'
   },
   {
     name: "Market Lunch",
@@ -15,7 +16,8 @@ var Model = {
     location: {
         lat: 38.886652,
         lng: -76.996438
-      }
+    },
+    FourSquareVenueID: '4ad20de2f964a52012df20e3'
   },
   {
     name: "Shaw's Tavern",
@@ -23,7 +25,8 @@ var Model = {
     location: {
         lat: 38.915118,
         lng: -77.019651
-      }
+    },
+      FourSquareVenueID: '4dfcd01aae6033b2a475c54d'
   },
   {
     name: "Tonic",
@@ -31,7 +34,8 @@ var Model = {
     location: {
         lat: 38.89808,
         lng: -77.046469
-      }
+    },
+    FourSquareVenueID: '49d1b87af964a520955b1fe3'
   },
   {
     name: "Daikaya",
@@ -39,7 +43,8 @@ var Model = {
     location: {
         lat: 38.898574,
         lng: -77.019638
-      }
+    },
+    FourSquareVenueID: '50905502d63e87c2d3448e35'
     }
     ],
   apiData: []
@@ -55,36 +60,41 @@ function Lists(data) {
    self.name = ko.observable(data.name);
    self.address = ko.observable(data.address);
    self.location = ko.observable(data.location);
+   self.FourSquareVenueID = ko.observable(data.FourSquareVenueID);
 };
-
 
 var map;
 var markers = [];
 var infoWindow;
-
 
 var viewModel = function() {
 
    var self = this;
    var marker;
    var searchResult;
+   var contentString = "";
+   var contentString1 = "";
+   var contentString2 = "";
+   infoWindow = new google.maps.InfoWindow();
+
 
    self.search_text = ko.observable('');
    self.nameList = ko.observableArray([]);
    self.showFilteredMarkers = ko.observable(); // names to store the filter
+   self.Venue = ko.observableArray([]);
 
 
    // Iterates through defaultData in Model and adds info to markers.
    Model.defaultData.forEach(function (names) {
-      self.nameList.push(new Lists(names));
+       self.nameList.push(new Lists(names));
    });
 
    // Filter based on user text / first matching item by name.
    self.filterNameList = ko.computed(function() {
 
-        if(!self.search_text()) {
+        if (!self.search_text()) {
             searchResult = self.nameList();
-        }else {
+        } else {
             searchResult = ko.utils.arrayFilter(self.nameList(), function (name) {
 
                 return (
@@ -104,32 +114,22 @@ var viewModel = function() {
             for ( i = 0; i < namesArray.length; i++) {
                 namesArray[i].marker.setVisible(false);
             }
-
             for ( i = 0; i < filteredSearchArray.length; i++) {
-
                 namesArray[i].marker.setVisible(true);
             }
-
         };
 
     // Generate marker and its other properties.
-    for(var i = 0; i < self.nameList().length; i++){
+        for(var i = 0; i < self.nameList().length; i++) {
 
-        marker = new google.maps.Marker({
-            map: map,
-            position: self.nameList()[i].location(),
-            title: self.nameList()[i].name(),
-            animation: google.maps.Animation.DROP
-        });
-        self.nameList()[i].marker = marker;
-
-        // InfoWindow content.
-        var popupContent = '<div id="iw-container">' +
-            '<div class="iw-title">' + self.nameList()[i].name() +'</div>' +
-            '</div>';
-
-            // Call infoWindowHandler to manage popup content.
-            populateInfoWindow(marker, popupContent);
+            marker = new google.maps.Marker({
+                map: map,
+                position: self.nameList()[i].location(),
+                title: self.nameList()[i].name(),
+                animation: google.maps.Animation.DROP,
+                id:i
+            });
+            self.nameList()[i].marker = marker;
 
             // Pushes all premade marker from for loop to markers array defined earlier.
             markers.push(marker);
@@ -137,26 +137,34 @@ var viewModel = function() {
             var icon1 = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
             var icon2 = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
 
-            marker.addListener('mouseover',function() {
+            marker.addListener('mouseover', function () {
                 this.setIcon(icon2);
             });
-            marker.addListener('mouseout', function() {
+            marker.addListener('mouseout', function () {
                 this.setIcon(icon1);
+            });
+
+            marker.addListener('click', function () {
+                var i = this.id;
+
+                contentString = "";
+                populateInfoWindow(this, contentString);
+                foursquareRequest(self.nameList()[i].FourSquareVenueID());
+
+                contentString = contentString1 + contentString2;
+
+                if (contentString != "") {
+                    infoWindow.setContent(contentString);
+                    infoWindow.open(map, this);
+                }
             });
         }
 
 
-
-        // Generate blank info object
-        infoWindow = new google.maps.InfoWindow();
-
-        // Populate one infowindow when one of the markers is clicked.
-        function populateInfoWindow(marker, popupContent) {
-             google.maps.event.addListener(marker, 'click', function () {
-             infoWindow.setContent(popupContent);
+    // Populate one infowindow when one of the markers is clicked.
+    function populateInfoWindow(marker, popupContent) {
 
              if (marker.getAnimation() !== null) {
-
                  this.setAnimation(null);
                  marker.setIcon(icon2);
                  infoWindow.close(map, this);
@@ -166,88 +174,77 @@ var viewModel = function() {
                       marker.setAnimation(null);
                   }, 2000);
                  marker.setIcon(icon1);
-                 infoWindow.open(map, this);
            }
 
            // Check if infowindow is already opened on this marker.
-           if (infoWindow.marker != marker) {
-                 infoWindow.setContent('');
+             if (infoWindow.marker != marker) {
                  infoWindow.marker = marker;
 
-               infoWindow.addListener('closeclick', function() {
-                   infoWindow.marker = null;
-               });
+                 var streetViewService = new google.maps.StreetViewService();
+                 var radius = 50;
+                 streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
 
-               var streetViewService = new google.maps.StreetViewService();
-               var radius = 50;
+                 function getStreetView(data, status) {
+                     contentString = "";
+                     if (status == google.maps.StreetViewStatus.OK) {
+                         var nearStreetViewLocation = data.location.latLng;
+                         var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
+                         var panoramaOptions = {
+                             position: nearStreetViewLocation,
+                             pov: {
+                                 heading: heading,
+                                 pitch: 30
+                             }
+                         };
+                         var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
 
-               // Add Streetview to the infowindow.
-               function getStreetView(data, status) {
-                   if (status == google.maps.StreetViewStatus.OK) {
-                       var nearStreetViewLocation = data.location.latLng;
-                       var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-                      //  infoWindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-                       var panoramaOptions = {
-                           position: nearStreetViewLocation,
-                           pov: {
-                             heading: heading,
-                             pitch: 30
-                           }
-                        };
-                       var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
-                    } else {
-                        infoWindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
-                    }
-               }
-              //  streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-              //  infoWindow.open(map, marker);
+                         return contentString1 = '<div><h2>' + marker.title + '</h2></div><div id="pano"></div>';
+
+                     } else {
+                         return contentString1 = '<div>' + marker.title + '</div>' + '<div>No Street View Found</div>';
+                     }
+                 }
+                 return contentString1 = '<div><h2>' + marker.title + '</h2></div><div id="pano"></div>';
            }
-       });
+
     }
 
     // Function to call the FourSquares API.
     // https://discussions.udacity.com/t/how-do-i-use-foursquare-api/210274/5
     // https://discussions.udacity.com/t/help-with-adding-the-foursquare-api/256786/6
-    var foursquareRequest = function(marker) {
-        var apiURL = 'https://api.foursquare.com/v2/venues/search';
-        var foursquareClientID = 'AGUWVQNXJEU211JMQVKINHZOHLFB5B3OVL05ESNW0I1BPAGJ'
+    function foursquareRequest (VenueID) {
+
+        var apiURL = 'https://api.foursquare.com/v2/venues/';
+        var foursquareClientID = 'AGUWVQNXJEU211JMQVKINHZOHLFB5B3OVL05ESNW0I1BPAGJ';
         var foursquareSecret ='YG3GS11IP2TL2SBJFDUUKMXQ1ZSMK2RUTSXFVAM5OEQYNB4Z';
         var foursquareVersion = '20171221';
-        var venueFoursquareID = marker.id;
-        var foursquareURL = apiURL + venueFoursquareID + '?client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
+        var venueFoursquareID = VenueID;
+
+        apiUrl = apiURL + venueFoursquareID + '?client_id=' + foursquareClientID +
+            '&client_secret=' + foursquareSecret + '&v=' + foursquareVersion + '&limit=1';
+
 
         $.ajax({
-            url: foursquareURL,
-            dataType: 'json',
-            data: {
-                  client_id: foursquareClientID,
-                  client_secret: foursquareSecret,
-                  v: foursquareVersion,
-                  near: "Washington, DC",
-                  query: "restaurant",
-                  async: true
+            url: apiUrl,
+            async: false,
+            success: function (data) {
+
+                var rating = data.response.venue.rating ? data.response.venue.rating : "unavailable to show rating";
+                //var name = data.response.venue.categories.name ? data.response.venue.categories.name : "unavailable to show name";
+                var location = data.response.venue.location.address ? data.response.venue.location.address : "unavailable to show address";
+
+                return contentString2 = '<div><b><h4>' + 'Rating: ' + rating.toString() +
+                                        '</h4></b></div><div><h4>' + 'Address: ' + location + '</h4></div>';
+                //'<div><b>' + 'categories: ' + name + '</b></div>' +
             },
-            success: function(data) {
-                console.log(data);
-                var rating = data.response.venue.rating ? data.response.venue.rating: "unavailable to show rating";
-                var name =  data.response.venue.name ? data.response.venue.name: "unavailable to show name";
-                var location = data.response.venue.location.address ? data.response.venue.location.address: "unavailable to show location";
+            error: function (e) {
+                return contentString2 = '<h5>Foursquare data is unavailable.</h5>';
+            }
+        });
+        return contentString2;
 
-                this.infowindow.setContent('<div>' + '<b>' + name + '</b>' + '</div>' +
-                                          '<div>' + '<b>' + rating.toString() + '</b>' + '</div>' +
-                                          '<div>' + location + '</div>' +
-                                          '<div>' + marker.title +
-                                          '</div><div id="pano"></div>');
+    };
 
-                streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-
-                this.infowindow.open(map, marker);
-                }
-          }).fail(function(e) {
-                console.log(error)
-                this.infoWindow.setContent("<div><h4>Sorry. It's unavailable to load Foursquare info now.</h4></div>")
-          });
-      };
 };
 
 
